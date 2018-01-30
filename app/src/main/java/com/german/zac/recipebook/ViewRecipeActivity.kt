cf7f26f2148236780276efe98b2220f81c56b36f
zac <zacgemanz>
2018-01-30 14:05:59 -0500

    added recipe view post database pre database helper just starting hash

---
+++ app/src/main/java/com/german/zac/recipebook/ViewRecipeActivity.kt
@@ -0,0 +1,12 @@
+package com.german.zac.recipebook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRecipeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_recipe)
+    }
+}
